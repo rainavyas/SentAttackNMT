1864911c073ecfda47c43e20054dfe64acd33641
Vyas Raina <sayvaniar@gmail.com>
2022-04-05 01:53:11 +0100

    temp test auto

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ import torch.nn as nn
 import numpy as np
 from scipy.special import softmax
 from transformers import FSMTForConditionalGeneration, FSMTTokenizer
-from transformers import BertTokenizerFast
 
 class NMTSeq2Seq(nn.Module):
 
@@ -97,8 +96,7 @@ class LangSentClassifier():
 
     def __init__(self, mname='blanchefort/rubert-base-cased-sentiment-rusentiment'):
 
-        # self.tokenizer = AutoTokenizer.from_pretrained(mname)
-        self.tokenizer = BertTokenizerFast.from_pretrained('blanchefort/rubert-base-cased-sentiment-rusentiment')
+        self.tokenizer = AutoTokenizer.from_pretrained(mname)
         self.model = AutoModelForSequenceClassification.from_pretrained(mname)
     
     @torch.no_grad()
